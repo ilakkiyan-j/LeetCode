ffa9b0831762e57563942790d8d5557b02345ba7
Ilakkiyan J <ilakkiyanj03@gmail.com>
2024-07-09 13:16:45 +0530

    Create 2390.Removing stars from String.java

---
+++ 2390.Removing stars from String.java
@@ -0,0 +1,10 @@
+public String removeStars(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='*')st.pop();
+            else st.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty())sb.append(st.pop());
+        return new String(sb.reverse());
+    }
