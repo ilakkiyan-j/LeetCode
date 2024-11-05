848345c1c1f487f5e64346ed82b6cc4bf3012a9a
Ilakkiyan J <ilakkiyanj03@gmail.com>
2024-11-05 21:57:23 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 39.09%] - LeetPush

---
+++ 74-Search-a-2D-Matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for(int i=0;i<m;i++){
+           if(target>= matrix[i][0] && target<=matrix[i][n-1]){
+            int index = binary_search(begin(matrix[i]),end(matrix[i]),target);
+            return index;
+           }
+        }
+        return 0;
+                }
+};
