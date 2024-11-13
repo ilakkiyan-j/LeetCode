3343ea5069bf328c44cb2e2142687fd841a71daf
Ilakkiyan J <ilakkiyanj03@gmail.com>
2024-11-13 21:29:07 +0530

    [2563] [Time Beats: 98.81%] [Memory Beats: 18.39%] - LeetPush

--- 2563-Count-the-Number-of-Fair-Pairs.cpp
+++ 2563-Count-the-Number-of-Fair-Pairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    long long countFairPairs(vector<int>& v, int lower, int upper) {
-        long long ans = 0;
-        sort(v.begin(), v.end());
-
-        for (int i = 0; i < v.size() - 1; i++) {
-            auto up = upper_bound(v.begin() + i + 1, v.end(), upper - v[i]);
-            auto low = lower_bound(v.begin() + i + 1, v.end(), lower - v[i]);
-            ans += (up - low);
-        }
-        return ans;
-    }
+      long long countLess(vector<int>& nums, int val) {
+    long long res = 0;
+    for (int i = 0, j = nums.size() - 1; i < j; ++i) {
+        while(i < j && nums[i] + nums[j] > val)
+            --j;
+        res += j - i;
+    }        
+    return res;
+}
+long long countFairPairs(vector<int>& nums, int lower, int upper) {
+    sort(begin(nums), end(nums));
+    return countLess(nums, upper) - countLess(nums, lower - 1);
+}
 };
