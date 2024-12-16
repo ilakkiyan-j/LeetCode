e48366107eeeec34905e3e47c7f6f633b0b34203
Ilakkiyan J <ilakkiyanj03@gmail.com>
2024-12-16 22:21:24 +0530

    [3264] [Time Beats: 30.95%] [Memory Beats: 29.75%] - LeetPush

---
+++ 3264-Final-Array-State-After-K-Multiplication-Operations-I.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> q;
+        int n = nums.size();
+        for(int x=0;x<n;x++){
+            q.push({nums[x],x});
+        }
+        for(int i=0;i<k;i++){
+             auto[num,idx] = q.top();
+             q.pop();
+             q.push({num*multiplier,idx});
+        }
+        for(int x=0;x<n;x++){
+             auto[num,idx] = q.top();
+             q.pop();
+             nums[idx]=num;
+        }
+        return nums;
+    }
+};
